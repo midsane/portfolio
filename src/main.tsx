99947a544c41cf023044e047bb4257a5ca7f2cd6
midsane <adityaraj10544@gmail.com>
2026-07-11 22:51:45 +0530

    changed app-wide font to jet brains nerd-font mono

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+// Import regular and bold weights
+import "@fontsource/jetbrains-mono/400.css"; 
+import "@fontsource/jetbrains-mono/700.css";
+// If you want italics as well:
+import "@fontsource/jetbrains-mono/400-italic.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
